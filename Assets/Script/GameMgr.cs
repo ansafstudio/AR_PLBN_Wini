2d8833a48a2be78ced69afdec415ebc35bd73847
Muhammad Anwar Safei <muhammad.anwar.safei@gmail.com>
2018-08-09 12:06:50 +0700

    Initial Commit
    Initial Commit

---
+++ Assets/Script/GameMgr.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour {
+    public GameObject[] Models;
+    bool isZoomIn;
+    bool isZoomOut;
+
+    bool isMoveLeft;
+    bool isMoveRight;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Zoom
+        for (int i = 0; i < Models.Length; i++)
+        {
+            if (isZoomIn)
+            {
+                Models[i].transform.localScale += new Vector3(0.0001f, 0.0001f, 0.0001f);
+            }
+            else if (isZoomOut)
+            {
+                Models[i].transform.localScale -= new Vector3(0.0001f, 0.0001f, 0.0001f);
+            }
+
+            if (isMoveLeft)
+            {
+                Models[i].transform.Translate(Vector3.right * Time.deltaTime * 5);
+            }
+            else if (isMoveRight)
+            {
+                Models[i].transform.Translate(Vector3.left * Time.deltaTime * 5);
+            }
+        }
+    }
+
+    //Zoom
+    public void ZoomINPressed()
+    {
+        isZoomIn = true;
+    }
+    public void ZoomINReleased()
+    {
+        isZoomIn = false;
+    }
+
+    public void ZoomOUTPressed()
+    {
+        isZoomOut = true;
+    }
+    public void ZoomOUTReleased()
+    {
+        isZoomOut = false;
+    }
+
+    //Move
+    public void MoveLeftPressed()
+    {
+        isMoveLeft = true;
+    }
+    public void MoveLeftReleased()
+    {
+        isMoveLeft = false;
+    }
+
+    public void MoveRightPressed()
+    {
+        isMoveRight = true;
+    }
+    public void MoveRightReleased()
+    {
+        isMoveRight = false;
+    }
+}
